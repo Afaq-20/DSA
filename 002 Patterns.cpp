4807eafce1bc66a8758c03bd801fcae857d1e8bd
Afaq Ahmad <113855626+Afaq-20@users.noreply.github.com>
2025-07-21 18:08:38 +0500

    Add files via upload

---
+++ 002 Patterns.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n= 4;
+	
+	for(int i= 0;i<n;i++){
+		for(int j= 0;j<=4;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	
+	
+//	Practice Question
+
+	int a= 1;
+	int m= 3;
+	 for(int k= 0;k<m;k++){
+	 	for(int l= 0;l<m;l++){
+	 		cout<<a;
+	 		a++;
+		 }
+		 cout<<endl;
+	 }
+//////////////////////////////////
+
+
+//	Practice Question
+
+	int o = 6;
+	 for(int p= 0;p<o;p++){
+	 	for(int q= 0;q<=p;q++){
+	 		cout<<"*";
+		 }
+		 cout<<endl;
+	 }
+///////////////////////////////
+
+
+//	Practice Question
+
+	int b= 1;
+	int f= 5;
+	 for(int r= 0;r<f;r++){
+	 	for(int s= 0;s<=r;s++){
+	 		cout<<b;
+		}
+	 	b++;
+		cout<<endl;
+	 }
+//////////////////////////////////
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
