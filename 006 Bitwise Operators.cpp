cf392ec6f52eb781d7d9a9f6e33b05e241915c45
Afaq Ahmad <113855626+Afaq-20@users.noreply.github.com>
2025-08-02 10:05:04 +0500

    Add files via upload

---
+++ 006 Bitwise Operators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	
+	int a=8,b=1;
+	
+	cout<<"AND operator: "<<(a&b)<<endl;
+	
+	cout<<"OR operator: "<<(a|b)<<endl;
+	
+	cout<<"XOR operator: "<<(a^b)<<endl;
+	
+	cout<<"LEFT Bitwise operator: "<<(a<<b)<<endl;
+	
+	cout<<"RIGHT Bitwise operator: "<<(a>>b)<<endl;
+	
+	return 0;
+	
+}
